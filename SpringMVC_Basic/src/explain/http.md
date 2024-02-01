50c36bd1393e661e64edbaa46ac444f7cbd0f74c
진현규 <108057548+jinhyeonkyu@users.noreply.github.com>
2024-02-02 00:27:17 +0900

    Todo : http매핑2

---
+++ SpringMVC_Basic/src/explain/http.md
@@ -0,0 +1,6 @@
+## http요청 - 기본, 헤더 조회
+
+![img.png](img.png)
+
+
+## http요청 파라미터 - 쿼리 파라미터, HTML Form
---
+++ SpringMVC_Basic/src/main/java/hello/springmvc/basic/requestmapping/RequestParamController.java
@@ -0,0 +1,22 @@
+package hello.springmvc.basic.requestmapping;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+	@RequestMapping("/request-param-v1")
+	public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String username = request.getParameter("username");
+		String age = request.getParameter("age");
+		log.info("username={}, age={}", username, age);
+		response.getWriter().write("ok");
+	}
+}
